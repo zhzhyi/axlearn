update the test, and fix the exp2 overflow issue

axlearn/common/flash_attention/gpu_attention.py
@@ -43,7 +43,7 @@ import numpy as np
 # pytype: enable=import-error  # pylint: enable=import-error
 
 Tensor = jax.Array
-DEFAULT_MASK_VALUE= -0.7 * float(np.finfo(np.dtype("float32")).max)
+DEFAULT_MASK_VALUE= -0.1 * float(np.finfo(np.dtype("float32")).max)
 
 
 def _mha_forward_kernel(
@@ -102,7 +102,8 @@ def _mha_forward_kernel(
     q = pl.load(q_ref, (curr_q_slice, pl.dslice(None)))
     # TODO: fix the segment mask for the case where seq length is not the whole
     # context.
-
+    qk_scale = softmax_scale
+    qk_scale *= 1.44269504  # 1/log(2)
     # In FlashAttention algorithm 1 there are 2 loops: slow over tiles of kv (size
     # Bc == block_k here), and fast over blocks of q (size Br == block_q here).
     # Here we only loop over blocks of kv to process entire seq_len, the loop over
@@ -114,8 +115,8 @@ def _mha_forward_kernel(
         k = pl.load(k_ref, (curr_k_slice, pl.dslice(None)))
 
         qk = pl.dot(q, k.T)  # [block_q, block_k].
-        if softmax_scale != 1.:
-            qk *= softmax_scale
+        if qk_scale != 1.:
+            qk *= qk_scale
         if bias_type == "matrix":
             b = pl.load(b_ref,(curr_q_slice, curr_k_slice),)
             qk += b
@@ -131,9 +132,9 @@ def _mha_forward_kernel(
         qk = qk.astype(jnp.float32)
         m_curr = qk.max(axis=-1)
         m_next = jnp.maximum(m_curr, m_prev)
-        correction = jnp.exp(m_prev - m_next)
+        correction = jnp.exp2(m_prev - m_next)
         l_prev_corr = l_prev * correction
-        p =  jnp.exp(qk - m_next[:, None])
+        p =  jnp.exp2(qk - m_next[:, None])
         l_next = jnp.sum(p, axis=1) + l_prev_corr
         l_rcp = 1.0 / l_next
         p = p * l_rcp[:, None]

axlearn/common/flash_attention/gpu_attention_test.py
@@ -24,6 +24,7 @@ import pytest
 
 from axlearn.common.flash_attention.gpu_attention import flash_attention
 from axlearn.common.flash_attention.utils import mha_reference
+from jax.experimental.pallas.ops.attention import mha as pallas_mha
 
 
 @pytest.mark.parametrize(
@@ -41,7 +42,7 @@ from axlearn.common.flash_attention.utils import mha_reference
 @pytest.mark.parametrize("use_fwd", [True, False])
 @pytest.mark.parametrize("causal", [True, False])
 @pytest.mark.parametrize("sm_scale", [1.0, 0.123])
-@pytest.mark.parametrize("bias_type", ["none",])
+@pytest.mark.parametrize("bias_type", ["none","matrix"])
 def test_fwd_against_ref(
     batch_size: int,
     seq_len: int,
@@ -101,7 +102,7 @@ def test_fwd_against_ref(
         (2, 8, 384, 64),
     ],
 )
-@pytest.mark.parametrize("bias_type", ["none",])
+@pytest.mark.parametrize("bias_type", ["none","matrix"])
 @pytest.mark.parametrize("block_size", [128, 64])
 @pytest.mark.parametrize("causal", [True, False])
 def test_bwd_against_ref(
@@ -156,4 +157,60 @@ def test_bwd_against_ref(
     # Compare gradients.
     jax_grads = jax.grad(fn, argnums=(0, 1, 2))(q, k, v, bias)
     jax_ref_grads = jax.grad(ref_fn, argnums=(0, 1, 2))(q, k, v, bias)
+    chex.assert_trees_all_close(jax_grads, jax_ref_grads, atol=0.05)
+    
+    
+@pytest.mark.parametrize(
+    "batch_size,num_heads,seq_len,per_head_dim",
+    [
+        (2, 2, 384, 64),
+    ],
+)
+@pytest.mark.parametrize("block_size", [128])
+@pytest.mark.parametrize("causal", [True, False])
+def validate_triton_with_pallas(
+    batch_size: int,
+    num_heads: int,
+    seq_len: int,
+    per_head_dim: int,
+    block_size: int,
+    causal: bool,
+):
+    q = jax.random.normal(
+        jax.random.PRNGKey(0), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
+    )
+    k = jax.random.normal(
+        jax.random.PRNGKey(1), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
+    )
+    v = jax.random.normal(
+        jax.random.PRNGKey(2), (batch_size, seq_len, num_heads, per_head_dim), dtype=jnp.float16
+    )
+
+
+    assert str(q.device()) == "cuda:0"
+    sm_scale = q.shape[-1] ** -0.5
+
+    # Compare outputs.
+    jax_out = pallas_mha(q, k, v, segment_ids=None, causal=causal, sm_scale=sm_scale)
+    jax_ref_out = mha_reference(q, k, v, causal=causal, softmax_scale=sm_scale)
+    chex.assert_trees_all_close(jax_out, jax_ref_out, atol=0.005)
+    def fn(q, k, v):
+        return pallas_mha(
+            q,
+            k,
+            v,
+            segment_ids=None,
+            causal=causal,
+            sm_scale=sm_scale,
+            block_q=block_size,
+            block_k=block_size,
+        ).sum()
+
+    def ref_fn(q, k, v):
+        # Pallas does not support bias, so we hardcode it to None.
+        return mha_reference(q, k, v, None, causal=causal, softmax_scale=sm_scale).sum()
+
+    # Compare gradients.
+    jax_grads = jax.grad(fn, argnums=(0, 1, 2))(q, k, v)
+    jax_ref_grads = jax.grad(ref_fn, argnums=(0, 1, 2))(q, k, v)
     chex.assert_trees_all_close(jax_grads, jax_ref_grads, atol=0.05)